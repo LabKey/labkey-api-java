74fc3da025388eae23470768d5dd5e1d7e10005e
Adam Rauch <adam@labkey.com>
2010-04-22 03:04:20 +0000

    Update licenses
    SVN r13564 |2010-04-22 03:04:20 +0000

--- java/src/org/labkey/remoteapi/query/jdbc/LabKeyDatabaseMetaData.java
+++ java/src/org/labkey/remoteapi/query/jdbc/LabKeyDatabaseMetaData.java
@@ -1,7 +1,20 @@
+/*
+ * Copyright (c) 2010 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.labkey.remoteapi.query.jdbc;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.labkey.remoteapi.CommandException;
 import org.labkey.remoteapi.query.*;
 import org.labkey.remoteapi.security.GetContainersCommand;
@@ -11,7 +24,6 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
