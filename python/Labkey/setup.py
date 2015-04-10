bf8da5229d84f98651acb9a289cf7da72e808e18
Adam Rauch <adam@labkey.com>
2015-04-10 18:49:19 +0000

    Update licenses
    SVN r37448 |2015-04-10 18:49:19 +0000

--- python/Labkey/setup.py
+++ python/Labkey/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Copyright (c) 2011-2014 LabKey Corporation
+# Copyright (c) 2011-2015 LabKey Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
